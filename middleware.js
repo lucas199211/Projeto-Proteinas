621e6d0cbe87b9ab16c95d0d693ec7a2d8da6b2e
lucas199211 <79679380+lucas199211@users.noreply.github.com>
2026-07-23 15:21:40 -0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,24 @@
+export const config = {
+  matcher: '/(.*)',
+};
+
+export default function middleware(request) {
+  const auth = request.headers.get('authorization');
+
+  if (auth) {
+    const [, encoded] = auth.split(' ');
+    const [user, pwd] = atob(encoded).split(':');
+
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASS) {
+      return;
+    }
+  }
+
+  return new Response('Autenticação necessária.', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Área restrita"',
+    },
+  });
+}
 
